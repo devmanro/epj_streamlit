b61a60c957a0de678f3988ffedcc9c7629e5caa0
devmanro <66820352+devmanro@users.noreply.github.com>
2026-01-05 12:14:34 +0100

    edited ship name on gendebq

--- modules/genDebarq.py
+++ modules/genDebarq.py
@@ -210,9 +210,9 @@ def gen_table(filepath=None):
     ws = wb.active
     ws.title = f"{file_name_only}"
 
+    ship_name_placeholder=f"{SHIP NAME: file_name_only}"
     ws.merge_cells('A1:G1')
-    SHIP_NAME_PLACEHOLDER=f"{SHIP NAME: file_name_only}"
-    ws['A1'].value = SHIP_NAME_PLACEHOLDER
+    ws['A1'].value = ship_name_placeholder
     ws['A1'].font = Font(bold=True, size=14)
 
     specific_keywords = ["BOBINE", "TUBE", "CTP", "BIGBAG"]
