9283df8ec1b4d1a79f43332992534a4ac191d525
devmanro <66820352+devmanro@users.noreply.github.com>
2026-01-13 00:20:29 +0100

    fixed staff_template.csv

--- assets/constants/constants.py
+++ assets/constants/constants.py
@@ -1,7 +1,11 @@
 PATH_BRDX="reports/bordereaux"
 PATH_PVS="reports/pvs"
 PATH_TEMPLATES="assets/templates"
+FIXED_STAFF_PATH="assets/templates/staff_template.csv"
+
+
 PATH_DEBRQ = "data/debarqs"
 UPLOAD_DIR = "data/uploads"
 DB_PATH="data/database.xlsx"
 MAP_IMAGE_PATH = "assets/map/port_map.png"
+
