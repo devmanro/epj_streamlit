ceef08caa4a6efd1dfc0b0e3ad739cfb01d69156
developer <devman@gmail.com>
2026-02-19 17:33:01 +0100

    req fixed

--- modules/staff_manager.py
+++ modules/staff_manager.py
@@ -184,7 +184,7 @@ def staff_m():
 
                 with col_btn_3:
                     towrap_new = io.BytesIO()
-                    edited_stage.to_excel(towrap_new, index=False, engine='xlsxwriter')
+                    gitedited_stage.to_excel(towrap_new, index=False, engine='xlsxwriter')
                     st.download_button(
                         label="🖨️ Print / Export Staging",
                         data=towrap_new.getvalue(),
